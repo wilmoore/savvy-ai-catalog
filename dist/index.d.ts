932b9da8bb18a767ebcc319a7f8bf0d1fe9c7a8c
Wil (₩) Moore III <51953+wilmoore@users.noreply.github.com>
2026-01-19 00:03:58 -0500

    chore: add .gitignore and remove build artifacts

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { CatalogEntry, Catalog } from "./types.js";
-export type { CatalogStore } from "./store.js";
-export { JsonFileCatalogStore, createStore, DEFAULT_CATALOG_PATH } from "./store.js";
-export { CatalogService, createCatalogService } from "./catalog.js";
--- dist/index.js
+++
@@ -1,2 +0,0 @@
-export { JsonFileCatalogStore, createStore, DEFAULT_CATALOG_PATH } from "./store.js";
-export { CatalogService, createCatalogService } from "./catalog.js";
